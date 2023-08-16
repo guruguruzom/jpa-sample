554ced3ed92f8eae4a81da46206da36a38d90e12
guruguruzom <guruguruzom@gmail.com>
2023-08-16 22:54:30 +0900

    db connect yml 변경

--- src/main/java/com/example/jpasample/JpaSampleApplication.java
+++ src/main/java/com/example/jpasample/JpaSampleApplication.java
@@ -12,24 +12,24 @@ import java.sql.SQLException;
 public class JpaSampleApplication {
 
     public static void main(String[] args) {
-        //SpringApplication.run(JpaSampleApplication.class, args);
+        SpringApplication.run(JpaSampleApplication.class, args);
 
 
-        String url = "jdbc:mariadb://localhost:3306/jpa_sample";
-        String username = "root";
-        String password = "qwe123!@#";
-        //connection 비용 자체가 비쌈
-        try(Connection connection = DriverManager.getConnection(url,username,password)){
-            System.out.println("Connection :" + connection);
-            String sql = "CREATE TABLE account (id int, user varchar(255), password varchar(255));";
-            //반복적 코드 발생
-            //lazy loading 사용 자체가 어려움
-            try(PreparedStatement statement = connection.prepareStatement(sql)) {
-                statement.execute();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+//
+//        String username = "root";
+//        String password = "qwe123!@#";
+//        //connection 비용 자체가 비쌈
+//        try(Connection connection = DriverManager.getConnection(url,username,password)){
+//            System.out.println("Connection :" + connection);
+//            String sql = "CREATE TABLE account (id int, user varchar(255), password varchar(255));";
+//            //반복적 코드 발생
+//            //lazy loading 사용 자체가 어려움
+//            try(PreparedStatement statement = connection.prepareStatement(sql)) {
+//                statement.execute();
+//            }
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
 }
